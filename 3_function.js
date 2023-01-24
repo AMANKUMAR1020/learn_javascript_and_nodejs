f3aeb4d1085b441dcfbcb2f7316493d6b0b8327a
AMANKUMAR1020 <92974468+AMANKUMAR1020@users.noreply.github.com>
2023-01-24 12:03:14 -0800

    Add files via upload

---
+++ 3_function.js
@@ -0,0 +1,35 @@
+//engine - v8
+
+/*
+"use strict"
+function add(num1,num2){
+return num1+num2;
+} 
+let x = 9;
+let y = 8;
+
+let result = add(9,8);
+
+console.log(result);
+*/
+/*
+"use strict"
+
+let add = function(x,y) //this is anonymous function
+{
+    return x+y;
+}
+let res = add(5,4);
+
+console.log(res);
+*/
+/*
+"use strict"
+
+let add = (x,y) => x + y; //this is arrow function("=>") because we remove "function" 
+                                 // keyword also no need to 
+                                 // write 'return'
+
+let res = add(5,4);
+console.log(res);
+*/
